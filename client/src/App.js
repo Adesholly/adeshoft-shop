a18e8f9d5195ad36257da76a8fff47b1d2b58aaf
adesoft <adesholly11@gmail.com>
2022-03-08 04:27:34 +0100

    setting up the client environment

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className='App'>
+     <Navbar />
+     <main>
+      <h1>Main Shop</h1>
+     </main>
+    <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <footer>
+         <h1>Footer component</h1>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
