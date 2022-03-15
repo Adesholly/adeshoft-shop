0dcd6021bc92e23b937ec671100471c62f458708
adesoft <adesholly11@gmail.com>
2022-03-15 14:50:54 +0100

    Adding user as seeder script

---
+++ server/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    name: "admin adesholly",
+    email: "adesholly@example.com",
+    password: bcrypt.hashSync("12345678", 10),
+    isAdmin: true,
+  },
+  {
+    name: "marry woman",
+    email: "adesholly@example.com",
+    password: bcrypt.hashSync("12345678", 10),
+  },
+
+  {
+    name: "daniel man",
+    email: "adesholly@example.com",
+    password: bcrypt.hashSync("12345678", 10),
+  },
+]
