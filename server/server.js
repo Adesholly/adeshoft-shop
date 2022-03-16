734fc2dbdb5d35e108e7f25fc881d6e153f6cafa
adesoft <adesholly11@gmail.com>
2022-03-16 03:14:18 +0100

    creating the product route endpoint

--- server/server.js
+++ server/server.js
@@ -1,26 +1,15 @@
 import express from "express"
-import products from "./data/products.js"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import colors from "colors"
+import productRoute from "./route/productRoute.js"
 
 dotenv.config()
 
 connectDB()
 const app = express()
 
-app.get("", (req, res) => {
-  res.send("I am understanding the basics")
-})
-
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((item) => item._id === req.params.id)
-  res.json(product)
-})
+app.use("/api/products", productRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(
