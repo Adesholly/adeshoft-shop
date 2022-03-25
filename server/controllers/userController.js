32dc6f3cc350631904537a35b3599c76f7864072
adesoft <adesholly11@gmail.com>
2022-03-25 04:20:29 +0100

    User registration from the backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,8 @@ import asyncHandler from "express-async-handler"
 import User from "../models/userModel.js"
 import generateToken from "../utils/generateToken.js"
 
+//Authenticate whether the user has an account or not
+//For login information
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
@@ -21,6 +23,7 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+//User profile information
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
@@ -37,4 +40,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+//User Registration information
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error("User already exist")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid User credentials")
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("bearer")
+    req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1]
